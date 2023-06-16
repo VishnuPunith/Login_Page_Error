0ccec459df7391d6671a37cf4da4bb02317f1e65
ramesh1919 <ramesh.avula@vistafoundation.co.in>
2023-06-16 11:43:45 +0530

    Change

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,267 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="background">
+      <!-- <div class="header"> -->
+        <div class="navbar">
+        <a href="https://student.v-learning.in/"><img src="../src/assets/logolink.webp" class="image"></a>
+           <div class="nav">
+          <button type="button" class="button1">
+            <span>SignUp</span>
+          </button>
+          <button type="button" class="button2">
+            <span>Login</span>
+          </button>
+        </div>
+        </div>
+        <div class="patch">
+        <div class="login">
+      <img src="../src/assets/03.png" class="imglay">
+      <form @submit.prevent="submitSignupForm">
+        <div class="form-group">
+          <label for="firstname"></label>
+          <input type="firstname" id="firstname" placeholder="Enter the name" v-model="firstname" required />
+        </div>
+        <div class="form-group">
+        <label for="email"></label>
+        <input type="email" id="email" placeholder="Enter Email" v-model="email"/>
+      </div>
+      <div class="form-group">
+        <label for="password"></label>
+        <input type="password" id="password" placeholder="Type here Password" v-model="password" required />
+      </div>
+      <div class="form-group">
+        <label for="passwordConfirm"></label>
+        <input type="password" id="passwordConfirm" placeholder="Confirm Your Password" v-model="passwordConfirm" required/>
+      </div>
+      <div class="form-group">
+        <label for="phonenumber"></label>
+        <input type="text" id="phonenumber" placeholder="Enter your Contact Number" maxlength="10" v-model="phonenumber" required/>
+      </div>
+      <label>
+      <input type="checkbox" v-model="isChecked" />
+       I Agree and conditions
+    </label>
+      <button type="submit">Sign Up</button>
+    </form>
+    </div>
+  </div>
+      </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isChecked: false,
+      firstname:"",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      phonenumber:"",
+      loggedIn: false,
+      signedUp: false,
+      users: []
+    };
+  },
+  
+  methods: {
+    submitSignupForm() {
+      if (this.email && this.password && this.password === this.passwordConfirm) {
+        // Save the user's data to a "database" (in this case, a local variable)
+        this.users.push({ email: this.email, password: this.password });
+        alert("Signup successful! Please log in.");
+        this.signedUp = true;
+        this.email = "";
+        this.password = "";
+        this.passwordConfirm = "";
+      } else {
+        alert("Please enter valid credentials and matching passwords.");
+      }
+    },
+    },
   }
-}
-</script>
+    </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.signup-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+form {
+  width: 400px;
+  padding: 75px;
+  background-color: white;
+  border-radius: 4px;
+  margin-top: -1rem;
+}
+
+.form-group {
+  margin-bottom: 20px;
+  border-radius: 3rem;
+}
+
+label {
+  display: block;
+  font-weight: 600;
+  margin-bottom: 10px;
+}
+
+input[type="text"],
+input[type="firstname"],
+input[type="email"],
+input[type="password"],
+input[type="passwordConfirm"]
+{
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 15px;
+  font-style: Poppins;
+  border: 1px solid lightgray;
+}
+
+button[type="submit"] {
+  width: 90%;
+  padding: 10px;
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 19px;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.form-control {
+  margin-bottom: 10px;
+}
+
+.error {
+  border-color: red;
+}
+
+.small {
+  color: red;
+  font-size: 12px;
+}
+
+.password-input {
+  position: relative;
+}
+
+.hide-password {
+    border-radius: 3rem;
+    width: 16rem;
+    border: none;
+    font-size: 2rem;
+    height: 2rem;
+    line-height: 5rem;
+    margin-top: -1rem;
+    margin-left: 127px;
+}
+
+.password-input button {
+    position: absolute;
+    top: -9px;
+    color: #51d330;
+    right: -50px;
+    border-radius: 12px;
+}
+.background{
+  width: 100%;
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem;
+    max-width: 1200px;
+    margin: 0 auto;
+    background: #f4faff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.image {
+    height: 100%;
+    width: 15%;
+}
+  .login {
+    display: flex;
+    flex-direction: row;
+    height: 79%;
+    background: #efefef;
+    border-radius: 12px;
+}
+.email{
+    border-radius: 3rem;
+    width: 16rem;
+    border: none;
+    font-size: 2rem;
+    height: 2rem;
+    line-height: 5rem;
+    margin-top: 106px;
+    margin-left: 85px;
+}
+.button1 {
+    color: #3d68c9;
+    font-weight: 600;
+    font-size: 11px;
+    width: 94px;
+    margin-left: 1rem;
+    height: 25px;
+    border: solid 1px #0b59a5;
+    border-radius: 3rem;
+    padding: -14rem 0rem;
+    /* padding: 2rem 5rem; */
+    text-decoration: none;
+    background: rgba(0,0,0,0);
+    transition: 0.50s;
+}
+.button2 {
+    color: #3d68c9;
+    font-weight: 600;
+    font-size: 11px;
+    width: 94px;
+    margin-left: 1rem;
+    height: 25px;
+    border: solid 1px #0a4987;
+    border-radius: 3rem;
+    padding: -14rem 0rem;
+    /* padding: 2rem 5rem; */
+    text-decoration: none;
+    background: rgba(0,0,0,0);
+    transition: 0.50s;
+}
+.imglay{
+  width: 50%;
+}
+.nav {
+    display: flex;
+    margin-left: 47rem;
+    position: absolute;
+}
+.patch {
+    width: 76%;
+    margin-left: 7px;
+    min-height: 75vh;
+    padding: 2rem;
+    margin: 1rem 4px;
+    border-radius: 1rem;
+    background: #fff;
+    box-shadow: -5px 10px 15px rgba(11,89,165,.2);
+}
+.error {
+  color: red;
+  margin-top: 10px;
+}
+.navbar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.button1:hover,.button2:hover{
+  background-color: blue;
+  color: white;
 }
 </style>
